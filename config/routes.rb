b9d1e18b59a5555af88d955c06fef3c2ff1860f6
JackoDev <untaljacko@gmail.com>
2021-02-14 14:24:13 -0500

    [new] crud methods first part without create option

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   #resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/users', to: 'users#index'
+  get '/users/new', to: 'users#new'
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/users/:id/edit', to: 'users#edit', as: "edit_user"
+  #put '/users/:id', to: 'users#update'
+  patch '/users/:id', to: 'users#update'
+  delete '/users/:id', to: 'users#delete'
 end
